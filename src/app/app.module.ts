7b92f283b8ca0decb28dfe7df9a7af6afe083f8b
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2022-10-17 16:38:08 +0200

    feat: calendar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { CalendarComponent } from './calendar/calendar.component';
 import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent, SummaryComponent],
+  declarations: [
+    AppComponent,
+    MapComponent,
+    CalendarComponent,
+    SummaryComponent,
+  ],
   imports: [BrowserModule, IonicModule.forRoot(), LeafletModule],
   bootstrap: [AppComponent],
 })
