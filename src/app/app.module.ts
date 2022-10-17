26e7aba0a1aa176d8c59e11c65720febf275cb07
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2022-10-17 15:03:34 +0200

    feat: summary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { IonicModule } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { SummaryComponent } from './summary/summary.component';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
+  declarations: [AppComponent, MapComponent, SummaryComponent],
   imports: [BrowserModule, IonicModule.forRoot(), LeafletModule],
   bootstrap: [AppComponent],
 })
---
+++ src/app/summary/summary.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.scss'],
+})
+export class SummaryComponent {
+  public summary = $localize`Summary Component`;
+
+  constructor() {}
+}
