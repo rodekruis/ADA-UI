2a6cbde8955f0a4a41a83b7b86cce6ca9e1895b0
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2022-10-18 13:59:16 +0200

    feat: summary component chat with text

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -6,7 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent {
-  public summary = $localize`Summary Component`;
+  public events = [
+    {
+      name: 'Storm ANA',
+      country: 'Mozambique',
+      date: '24 Jan 2022',
+    },
+    {
+      name: 'Nyiragongo Eruption',
+      country: 'Democratic Republic of Congo',
+      date: '22 May 2021',
+    },
+    {
+      name: 'Beirut Explosion',
+      country: 'Lebanon',
+      date: '4 Aug 2020',
+    },
+    {
+      name: 'Typhoon Mangut',
+      country: 'Philippines',
+      date: '15 Sep 2018',
+    },
+  ];
 
   constructor() {}
 }
